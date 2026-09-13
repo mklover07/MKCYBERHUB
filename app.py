0ddedde50272246c4683b4f6b812c7caf9b63848
Manoj Sir <mklover07@gmail.com>
2026-09-13 20:31:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, jsonify, send_from_directory
-import os
+from flask import Flask, render_template, jsonify
 import random
 
 app = Flask(__name__)
@@ -26,10 +25,5 @@ def news():
     ]
     return jsonify({'news': random.choice(news_list)})
 
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
